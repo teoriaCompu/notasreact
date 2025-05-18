5d6cf091c9526c3954d0eef00b654bbabc410a2b
ANGY LORENA BAUTISTA SALAZAR <103713284+AnGISIta12@users.noreply.github.com>
2025-05-18 08:16:57 -0500

    Update EstudianteRepositorio.java

--- src/main/java/com/example/notasreact/repositorio/EstudianteRepositorio.java
+++ src/main/java/com/example/notasreact/repositorio/EstudianteRepositorio.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 public interface EstudianteRepositorio extends ReactiveCrudRepository<Estudiante, Long> {
+  Mono<Estudiante> findByCorreo(String correo);
 
 }
