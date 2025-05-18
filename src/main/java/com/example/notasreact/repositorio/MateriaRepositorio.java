73a4f705c4c88d017ef0a3249c0f628e6f788fca
ANGY LORENA BAUTISTA SALAZAR <103713284+AnGISIta12@users.noreply.github.com>
2025-05-18 09:27:18 -0500

    Update MateriaRepositorio.java

--- src/main/java/com/example/notasreact/repositorio/MateriaRepositorio.java
+++ src/main/java/com/example/notasreact/repositorio/MateriaRepositorio.java
@@ -3,6 +3,7 @@ package com.example.notasreact.repositorio;
 import com.example.notasreact.modelo.Materia;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface MateriaRepositorio extends ReactiveCrudRepository<Materia, Long> {
   Mono<Materia> findByNombre(String nombre);
